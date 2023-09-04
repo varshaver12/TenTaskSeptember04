7754858dcd4cdd4243fd09d16375bcaa385c7cb8
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-04 17:22:16 +0300

    Solution for task 5

---
+++ TenTaskSeptember04/TaskFive.swift
@@ -0,0 +1,11 @@
+/*
+ 
+ You are going to make toast fast, you think that you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.
+ 
+ */
+
+import Foundation
+
+func sixToast(_ num: Int) -> Int {
+    abs(num - 6)
+}
